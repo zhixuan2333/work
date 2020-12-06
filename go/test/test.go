00f5caa502716dea9de20c1f5df011320e543b7e
zhixuan666 <jinzhixuan666@gmail.com>
2020-12-06 10:59:43 +0900

    Update test.go

--- go/test/test.go
+++ go/test/test.go
@@ -1,29 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"io/ioutil"
-	"log"
-	"net/http"
-	"strings"
-)
-
 func main() {
-	client := &http.Client{}
-	var data = strings.NewReader(`{ "to": "U4af4980629...", "messages":[ { "type":"text", "text":"Hello, world1" }, { "type":"text", "text":"Hello, world2" } ] }`)
-	req, err := http.NewRequest("POST", "https://api.line.me/v2/bot/message/push", data)
-	if err != nil {
-		log.Fatal(err)
-	}
-	req.Header.Set("Content-Type", "application/json")
-	req.Header.Set("Authorization", "Bearer {channel access token}")
-	resp, err := client.Do(req)
-	if err != nil {
-		log.Fatal(err)
-	}
-	bodyText, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Printf("%s\n", bodyText)
+	fmt.Println("hello")
 }
