3bdbe52fc4179607033912d343c1244038edf88e
zhixuan666 <jinzhixuan666@gmail.com>
2020-12-31 09:35:23 +0900

    webpauto

--- go/webpauto/main.go
+++ go/webpauto/main.go
@@ -4,28 +4,33 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"os/exec"
 	"path/filepath"
 )
+
+const src = "./images"
 
 func main() {
 
 	list := WalkDir()
 
-	fmt.Println(list)
+	for _, l := range list {
+		out := Command(l, l+".webp")
+		fmt.Println(out)
+	}
 
 }
 
 // WalkDir range file
 func WalkDir() []string {
 	var list []string
-	err := filepath.Walk(".\\images",
+	err := filepath.Walk(src,
 		func(path string, info os.FileInfo, err error) error {
 			if err != nil {
 				return err
 			}
-			if !info.IsDir() {
+			if !info.IsDir() && path[len(path)-5:] != ".webp" {
 				list = append(list, path)
-				fmt.Println(path)
 			}
 			return nil
 		})
@@ -33,4 +38,15 @@ func WalkDir() []string {
 		log.Println(err)
 	}
 	return list
+}
+
+//Command usr cwebp to Create webp file
+func Command(src, dst string) []byte {
+	// Print Go Version
+	out, err := exec.Command("cwebp", src, "-o", dst).Output()
+	if err != nil {
+		log.Printf("Failed: %s\n", err.Error())
+	}
+
+	return out
 }
