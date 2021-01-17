ee6538d451fb66216dde311cb22db4810b78c1b8
zhixuan666 <59254886+zhixuan666@users.noreply.github.com>
2021-01-17 12:02:03 +0900

    test

--- go/test/main.go
+++ go/test/main.go
@@ -1,14 +1,64 @@
 package main
 
-import "context"
+import (
+	"context"
+	"log"
+	"net/http"
 
-type User struct {
-	DateOfBirth string `json:"date_of_birth,omitempty"`
-	FullName    string `json:"full_name,omitempty"`
-	Nickname    string `json:"nickname,omitempty"`
-}
+	firebase "firebase.google.com/go"
+	"firebase.google.com/go/db"
+	"github.com/gorilla/mux"
+	"google.golang.org/api/option"
+)
+
+var router = mux.NewRouter()
+var client *db.Client
 
 func main() {
-	ctx := context.Background()
+	client = New()
+
+	// server main method
+
+	router.HandleFunc("/", indexPageHandler)
+	router.HandleFunc("/internal", internalPageHandler)
+
+	router.HandleFunc("/login", loginHandler).Methods("POST")
+	router.HandleFunc("/logout", logoutHandler).Methods("POST")
+
+	http.Handle("/", router)
+	http.ListenAndServe(":80", nil)
 
 }
+
+// New init new fire client
+func New() *db.Client {
+	opt := option.WithCredentialsFile("go-db-48bf0-firebase-adminsdk-qjruj-e562bd373d.json")
+	config := &firebase.Config{
+		DatabaseURL: "https://go-db-48bf0-default-rtdb.firebaseio.com/",
+	}
+
+	ctx := context.Background()
+	app, err := firebase.NewApp(ctx, config, opt)
+	if err != nil {
+		log.Fatal(err)
+	}
+	client, err = app.Database(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println(client)
+
+	return client
+
+}
+
+// // NewUser Create new user func
+// func NewUser(newuser string) error {
+// 	ref := client.NewRef("users").Child(newuser.ID)
+// 	err := ref.Set(context.Background(), newuser)
+// 	if err != nil {
+// 		log.Printf("Creat new user filed: %e", err)
+// 		return err
+// 	}
+// 	return nil
+// }
