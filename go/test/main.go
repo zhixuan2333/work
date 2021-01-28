fda3b88b2bafec9e2d729462f8fbfbd81e1d4b5e
zhixuan666 <jinzhixuan666@gmail.com>
2021-01-28 21:54:22 +0900

    OK

--- go/test/main.go
+++ go/test/main.go
@@ -1,94 +1,37 @@
 package main
 
 import (
-	"fmt"
-	"io/ioutil"
-	"os"
-	"strings"
+	"context"
+	"log"
+
+	firebase "firebase.google.com/go"
+	"google.golang.org/api/option"
 )
 
-//获取指定目录下的所有文件和目录
-func GetFilesAndDirs(dirPth string) (files []string, dirs []string, err error) {
-	dir, err := ioutil.ReadDir(dirPth)
-	if err != nil {
-		return nil, nil, err
-	}
-
-	PthSep := string(os.PathSeparator)
-	//suffix = strings.ToUpper(suffix) //忽略后缀匹配的大小写
-
-	for _, fi := range dir {
-		if fi.IsDir() { // 目录, 递归遍历
-			dirs = append(dirs, dirPth+PthSep+fi.Name())
-			GetFilesAndDirs(dirPth + PthSep + fi.Name())
-		} else {
-			// 过滤指定格式
-			ok := strings.HasSuffix(fi.Name(), ".go")
-			if ok {
-				files = append(files, dirPth+PthSep+fi.Name())
-			}
-		}
-	}
-
-	return files, dirs, nil
-}
-
-//获取指定目录下的所有文件,包含子目录下的文件
-func GetAllFiles(dirPth string) (files []string, err error) {
-	var dirs []string
-	dir, err := ioutil.ReadDir(dirPth)
-	if err != nil {
-		return nil, err
-	}
-
-	PthSep := string(os.PathSeparator)
-	//suffix = strings.ToUpper(suffix) //忽略后缀匹配的大小写
-
-	for _, fi := range dir {
-		if fi.IsDir() { // 目录, 递归遍历
-			dirs = append(dirs, dirPth+PthSep+fi.Name())
-			GetAllFiles(dirPth + PthSep + fi.Name())
-		} else {
-			// 过滤指定格式
-			ok := strings.HasSuffix(fi.Name(), ".go")
-			if ok {
-				files = append(files, dirPth+PthSep+fi.Name())
-			}
-		}
-	}
-
-	// 读取子目录下文件
-	for _, table := range dirs {
-		temp, _ := GetAllFiles(table)
-		for _, temp1 := range temp {
-			files = append(files, temp1)
-		}
-	}
-
-	return files, nil
-}
-
 func main() {
-	files, dirs, _ := GetFilesAndDirs("./")
 
-	for _, dir := range dirs {
-		fmt.Printf("获取的文件夹为[%s]\n", dir)
+	config := &firebase.Config{
+		StorageBucket: "test-fb724.appspot.com",
+	}
+	opt := option.WithCredentialsFile("test-fb724-firebase-adminsdk-dcryi-81e7333440.json")
+	app, err := firebase.NewApp(context.Background(), config, opt)
+	if err != nil {
+		log.Fatalln(err)
 	}
 
-	for _, table := range dirs {
-		temp, _, _ := GetFilesAndDirs(table)
-		for _, temp1 := range temp {
-			files = append(files, temp1)
-		}
+	client, err := app.Storage(context.Background())
+	if err != nil {
+		log.Fatalln(err)
 	}
 
-	for _, table1 := range files {
-		fmt.Printf("获取的文件为[%s]\n", table1)
+	bucket, err := client.DefaultBucket()
+	if err != nil {
+		log.Fatalln(err)
 	}
+	// 'bucket' is an object defined in the cloud.google.com/go/storage package.
+	// See https://godoc.org/cloud.google.com/go/storage#BucketHandle
+	// for more details.
+	// [END cloud_storage_golang]
 
-	fmt.Printf("=======================================\n")
-	xfiles, _ := GetAllFiles("./")
-	for _, file := range xfiles {
-		fmt.Printf("获取的文件为[%s]\n", file)
-	}
+	log.Printf("Created bucket handle: %v\n", bucket)
 }
