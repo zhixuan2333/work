ce639cc10cbfc73c4a979d8039c302821ccadf04
zhixuan666 <jinzhixuan666@gmail.com>
2021-02-01 22:26:50 +0900

    Update entrypoint.sh

--- go/mc-heroku/entrypoint.sh
+++ go/mc-heroku/entrypoint.sh
@@ -4,10 +4,13 @@ echo | frpc -f 4d5d9503a4164891:852776 &
 
 mkdir /minecraft/app
 
+git init
+git remote add origin git@github.com:zhixuan666/mc-heroku.git
+git fetch
+git pull origin master
+
 echo "$(ls /minecraft)"
 echo "$(ls /minecraft/app)"
-
-echo | git clone git@github.com:zhixuan666/mc-heroku.git /minecraft/app
 
 java -Xms1024M -Xmx1024M -jar server.jar nogui
 
