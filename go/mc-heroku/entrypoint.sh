41b260d77f38682d96295a3d9b340e2e3c5c5a73
zhixuan666 <jinzhixuan666@gmail.com>
2021-02-01 21:20:36 +0900

    Update entrypoint.sh

--- go/mc-heroku/entrypoint.sh
+++ go/mc-heroku/entrypoint.sh
@@ -4,6 +4,9 @@ echo | frpc -f 4d5d9503a4164891:852776 &
 
 mkdir /minecraft/app
 
+echo "$(ls /minecraft)"
+echo "$(ls /minecraft/app)"
+
 echo | git clone git@github.com:zhixuan666/mc-heroku.git /minecraft/app
 
 java -Xms1024M -Xmx1024M -jar server.jar nogui
