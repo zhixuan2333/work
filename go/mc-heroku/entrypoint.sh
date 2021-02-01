bd6c6ed7a204626ef316ed74adcbe12d4e2178cc
zhixuan666 <jinzhixuan666@gmail.com>
2021-02-01 21:15:49 +0900

    OK

--- go/mc-heroku/entrypoint.sh
+++ go/mc-heroku/entrypoint.sh
@@ -2,6 +2,8 @@
 
 echo | frpc -f 4d5d9503a4164891:852776 &
 
+mkdir /minecraft/app
+
 echo | git clone git@github.com:zhixuan666/mc-heroku.git /minecraft/app
 
 java -Xms1024M -Xmx1024M -jar server.jar nogui
