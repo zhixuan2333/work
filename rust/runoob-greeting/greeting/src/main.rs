c0e953c47341bc1ed720873c0cd861ed650169f8
zhixuan <jinzhixuan666@gmail.com>
2021-04-09 13:05:47 +0900

    debug init

--- rust/runoob-greeting/greeting/src/main.rs
+++ rust/runoob-greeting/greeting/src/main.rs
@@ -1,4 +1,12 @@
 fn main() {
-    let a = 12;
-    println!("a is {}, again {}", a, a);
+    let list = [50, 100, 150, 200];
+    let mut i = 0;
+    let index = loop {
+        let ch = list[i];
+        if ch == 150 {
+            break i+1;
+        }
+        i += 1
+    };
+    println!("\'150\' index is {}", index)
 }
